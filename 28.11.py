c626910ab0adb9f71204cc2cc49cf50f3c9eaa4e
hafeeshacoder <fathimahafeesha632@gmail.com>
2024-11-29 10:19:19 +0530

    Add files via upload

---
+++ 28.11.py
@@ -0,0 +1,47 @@
+class Employee:
+    def getEmployeeInfo(self):
+        self.id=input("Enter the id:")
+        self.name=input("enter the name")
+    def displayEmployeeInfo(self):
+        print("ID=",self.id,"\nName=",self.name)
+class Perks(Employee):
+    def getDetails(self):
+        self.getEmployeeInfo()
+        self.sal=int(input("Enter the salary"))
+    def displayInfo(self):
+        self.displayEmployeeInfo()
+        print("salary=",self.sal)
+
+
+p=Perks()
+p.getDetails()
+p.displayInfo()
+
+
+class Inventory:
+    def _init_(self,prodID,prodName,prodCount):
+        self.prodID=prodID
+        self.prodName=prodName
+        self.prodCount=prodCount
+    def display(self):
+        print("Product ID:", self.prodID)
+        print("Product Name:", self.prodName)
+        print("Prodyct Count", self.prodCount)
+s=Show("E24AI012","Printer",100)
+s.display()
+
+
+
+
+class Inventory:
+    def _init_(self,prodID,prodName,prodCount):
+        self.prodID=prodID
+        self.prodName=prodName
+        self.prodCount=prodCount
+class Show(Inventory):
+    def display(self):
+        print("Product ID:", self.prodID)
+        print("Product Name:", self.prodName)
+        print("Prodyct Count", self.prodCount)
+s=Show("E24AI012","Printer",100)
+s.display()
